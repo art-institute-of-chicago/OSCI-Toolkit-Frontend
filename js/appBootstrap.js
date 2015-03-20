3c4c98ade4c135d1fe959d18c0c7d7920ed7572d
surreal8 <tinashah8@gmail.com>
2015-03-20 16:40:30 -0500

    changes for pushstate
    changes for pushstate and browser compatibility

--- js/appBootstrap.js
+++ js/appBootstrap.js
@@ -34,8 +34,26 @@ app = {
 	},
 
 	run : function() {
+		
+		var getUrlPath = this.views.app.$el.prop('baseURI');
+		if (!getUrlPath) { //for I.E., doesn't support baseURI
+			getUrlPath = document.URL;
+		}
+		
+		if (window.location.hash) {
+			//if there's a # in the url replace with /
+			getUrlPath = getUrlPath.replace("#", "/");
+		}
+		var pathArray = getUrlPath.split('/');
+		var rootPath = '';
+		
+		for (i=3; i < 6; i++) {
+			rootPath += "/";
+			rootPath += pathArray[i];
+		}
+		
 		// load package document
 		this.models.docPackage = new OsciTk.models.Package({url: this.config.get('packageUrl')});
-		Backbone.history.start();
+		Backbone.history.start({pushState:true, root: rootPath});
 	}
 };
